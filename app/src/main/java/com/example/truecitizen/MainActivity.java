7455c10b193a2c693b81dca624dbed5232ff5e24
healym3 <healym3@gator.uhd.edu>
2022-02-08 19:33:03 -0600

    Checks answers and now Prev and Next buttons are functional.

--- app/src/main/java/com/example/truecitizen/MainActivity.java
+++ app/src/main/java/com/example/truecitizen/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import com.example.truecitizen.databinding.ActivityMainBinding;
 import com.example.truecitizen.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -20,12 +21,47 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question_government_feds, false),
             new Question(R.string.question_government_senators, true),
     };
+    private int currentQuestionIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.questionText.setText(questionBank[0].getAnswerResId());
+        binding.questionText.setText(questionBank[currentQuestionIndex].getAnswerResId());
+
+        binding.nextButton.setOnClickListener(view-> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.length;
+            updateQuestion();
+        });
+        binding.prevButton.setOnClickListener(view -> {
+            if (currentQuestionIndex > 0) {
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionBank.length; //decrementing
+                updateQuestion();
+            }
+
+        });
+        binding.trueButton.setOnClickListener(view -> checkAnswer(true));
+        binding.falseButton.setOnClickListener(view -> checkAnswer(false));
+    }
+
+    private void updateQuestion() {
+        binding.questionText.setText(questionBank[currentQuestionIndex].getAnswerResId());
+    }
+
+    private void checkAnswer(boolean userChoseCorrect) {
+        boolean answerIsCorrect = questionBank[currentQuestionIndex].isAnswerTrue();
+        int messageId;
+
+        if (answerIsCorrect == userChoseCorrect) {
+            messageId = R.string.correct_answer;
+        }else {
+            messageId = R.string.wrong_answer;
+        }
+
+        Snackbar.make(binding.imageView, messageId, Snackbar.LENGTH_SHORT)
+                .show();
+
+
     }
 }
